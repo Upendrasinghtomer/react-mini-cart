86084e329057631aa437cecd5d500ae0bc0cb1ed
Upendra Tomar <tomer.upendra01@gmail.com>
2022-04-28 19:30:38 +0530

    devtools removed for production

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,6 +11,7 @@ const store = configureStore({
     categories: categoriesReducer,
     products: productsReducer,
   },
+  devTools: process.env.NODE_ENV !== "production",
 });
 
 export default store;
